c488974394a28b79327e32c931262d50b6e0fa24
LeonidGavrilov <leo2000--10@mail.ru>
2022-02-03 02:37:35 +0300

    multer_setting

--- multer/app.js
+++ multer/app.js
@@ -3,10 +3,31 @@ const multer  = require("multer");
   
 const app = express();
  
-const upload = multer({dest:"uploads"});
+const storageConfig = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) =>{
+        cb(null, file.originalname);
+    }
+});
+// определение фильтра
+const fileFilter = (req, file, cb) => {
+  
+    if(file.mimetype === "image/png" || 
+    file.mimetype === "image/jpg"|| 
+    file.mimetype === "image/jpeg"){
+        cb(null, true);
+    }
+    else{
+        cb(null, false);
+    }
+ }
+ 
 app.use(express.static(__dirname));
  
-app.post("/upload", upload.single("filedata"), function (req, res, next) {
+app.use(multer({storage:storageConfig, fileFilter: fileFilter}).single("filedata"));
+app.post("/upload", function (req, res, next) {
    
     let filedata = req.file;
  
@@ -16,4 +37,4 @@ app.post("/upload", upload.single("filedata"), function (req, res, next) {
     else
         res.send("Файл загружен");
 });
-app.listen(3000);
+app.listen(3000, ()=>{console.log("Server started");});
