e36773ba9c947e92fe6eb9db88cef605304a4f26
LeonidGavrilov <leo2000--10@mail.ru>
2022-02-02 05:31:16 +0300

    expressMVC_mongodb

--- expressMVC/app.js
+++ expressMVC/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const userRouter = require("./routes/userRouter.js");
 const homeRouter = require("./routes/homeRouter.js");
@@ -6,11 +7,31 @@ const homeRouter = require("./routes/homeRouter.js");
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: false }));
  
-app.use("/users", userRouter);
+app.use("/users", userRouter);;
 app.use("/", homeRouter);
  
 app.use(function (req, res, next) {
-    res.status(404).send("Not Found");
+    res.status(404).send("Not Found")
 });
- 
-app.listen(3000);
+
+const password = "V8d-VCL-HgG-Vua";
+const dbName = "usersdb";
+const url =
+  "mongodb+srv://LeonidG:" + password + "@cluster0.r4e4w.mongodb.net/" + dbName;
+// подключение
+mongoose.connect(url, {
+  useUnifiedTopology: true,
+}, function(err){
+  if(err) return console.log(err);
+  app.listen(3000, function(){
+      console.log("Сервер ожидает подключения...");
+  });
+});
+
+
+// mongoose.connect("mongodb://localhost:27017/usersdb", { useUnifiedTopology: true }, function(err){
+//     if(err) return console.log(err);
+//     app.listen(3000, function(){
+//         console.log("Сервер ожидает подключения...");
+//     });
+// });
